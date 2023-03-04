c4ae2e76774d5ba6b5ca4ed3f756be662be5ad2d
Aingkaran <aingkaran1995@gmail.com>
2023-03-04 18:31:49 -0500

    add typography theme

--- CheckoutBot/src/main.jsx
+++ CheckoutBot/src/main.jsx
@@ -10,8 +10,31 @@ import { ProSidebarProvider } from 'react-pro-sidebar';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { teal } from '@mui/material/colors';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const theme = createTheme({
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: 'h2',
+          h2: 'h2',
+          h3: 'h2',
+          h4: 'h2',
+          h5: 'h2',
+          h6: 'h2',
+          subtitle1: 'h2',
+          subtitle2: 'h2',
+          body1: 'span',
+          body2: 'span',
+        },
+      },
+    },
+  },
 
   palette: {
     primary: {
--- CheckoutBot/src/scenes/account/index.jsx
+++ CheckoutBot/src/scenes/account/index.jsx
@@ -1,7 +1,28 @@
+import React, { useState } from 'react';
+import './style.css'
+import { Container, Box } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { useForm } from "react-hook-form";
+import { Input, TextField, Button, Typography } from '@mui/material';
+
 const Account = () => {
 
     return (
-        <div>Account</div>
+        <Box sx={{
+            display: "flex",
+
+        }}>
+            <Box sx={{
+                display: "flex",
+                flexDirection: "column"
+            }}>
+                <Typography variant="h5" color="black">SIGN IN</Typography>
+
+            </Box>
+            <Box>
+
+            </Box>
+        </Box>
     )
 
 }
